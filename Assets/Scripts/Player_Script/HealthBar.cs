b0eaeeacaa2cd9b305ff2deaebe99dccfdb33b84
Le Chanh An <161144795+LeChanhAn@users.noreply.github.com>
2024-11-03 15:15:03 +0700

    AAN-created UI and function of HealthBar and LifeCount

--- Assets/Scripts/Player_Script/HealthBar.cs
+++ Assets/Scripts/Player_Script/HealthBar.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using UnityEngine;
+using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image fillBar;
+    public float health;
+    //100 health => 1 fi;; amount
+    //45 health => 0.45 fill amount
+    public void LoseHealth(int value)
     {
-        
+        //Do nothing if you are out of health
+        if (health <= 0) 
+        {
+            return;
+        }
+        //Reduce the health
+        health -= value;
+        //Refresh the UI fillBar
+        fillBar.fillAmount = health/100;
+        //check if your health is zero or less => Dead
+        if (health <= 0)
+        {
+            Debug.Log("YOU DIED");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+            LoseHealth(25);
     }
 }
--- Assets/Scripts/Player_Script/LifeCount.cs
+++ Assets/Scripts/Player_Script/LifeCount.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeCount : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image[] lives;
+    public int livesRemaining;
 
-    // Update is called once per frame
-    void Update()
+    //2 lives - 2 images(1,2)
+    public void LoseLife()
     {
-        
+        //If no lives remaining do nothing
+        if(livesRemaining == 0)
+        {
+            return;
+        }
+        //Decrease the value of livesRemaining
+        livesRemaining--;
+        //Hide one of the life images
+        lives[livesRemaining].enabled = false;
+        //If we run out of lives we lose the game
+        if (livesRemaining == 0)
+        {
+            Debug.Log("YOU LOST");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            LoseLife();
     }
 }
