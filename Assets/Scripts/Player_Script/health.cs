d795cbbee78d1ed75f4ef3e152dddc47091fa641
An Vũ <erogami5437@gmail.com>
2024-11-14 14:56:43 +0700

    anvu_create health bar and lose health mechanic

---
+++ Assets/Scripts/Player_Script/health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthbar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead");
+            // Thêm logic chết tại đây (như kích hoạt animation, hủy object, v.v.)
+        }
+
+        healthbar.sizeDelta = new Vector2(currentHealth-10, healthbar.sizeDelta.y);
+    }
+}
